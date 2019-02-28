589bc37e6f4658c29d2c255ed51c38c466180c11
Javier Farre <jfarre90@gmail.com>
2019-02-28 11:16:38 +0000

    added DATABASEURL for the database reference once app goes live

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true); //this allows us to see what is happening at any given point
-mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect(process.env.DATABASEURL || 'mongodb://localhost/todo-api', {useNewUrlParser: true});
 
 mongoose.Promise = Promise; //This is just to allow us to use the promise syntax .then .catch
 
